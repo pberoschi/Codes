d8bbc2e5feb09a4d84837629f828466883c24edb
pberoschi <beroschi.peter@gmail.com>
2024-09-13 14:32:20 -0300

    .

--- 1 VSCode/2 WebScraping/Downloads_Videos.py
+++ 1 VSCode/2 WebScraping/Downloads_Videos.py
@@ -1,20 +1,3 @@
-# from pytube import YouTube
-
-# link = input("Cole o link do vídeo aqui: ")
-
-# yt=YouTube(link)
-
-# print("Baixando", yt.title)
-
-# resolucao = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
-
-# resolucao.download()
-
-# print("Download Concluído")
-
-
-
-
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
  
