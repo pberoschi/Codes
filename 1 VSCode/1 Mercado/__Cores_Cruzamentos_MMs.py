3e671034c9c5d1a7b6fe4780ca841341b9255cfc
pberoschi <beroschi.peter@gmail.com>
2024-10-22 11:30:35 -0300

    .

---
+++ 1 VSCode/1 Mercado/__Cores_Cruzamentos_MMs.py
@@ -0,0 +1,75 @@
+#https://www.youtube.com/watch?v=TERKvqfySYI&t=1067s
+
+import pyautogui as py
+import time
+from datetime import datetime
+import shutil
+import telepot
+from time import sleep
+
+x1 = 3646
+y1 = 822
+
+#py.moveTo(-1835,122) #compra e venda
+py.moveTo(x1, y1) #compra e venda
+
+# --------------------------------------------------------------------------------- #
+# Enviando imediatamente MSG de atual posição
+RGB = list(py.pixel(x1, y1))
+RGB = str(RGB[2])
+
+def wdo4R():
+    
+    filename = 'Negocios_cruzam.txt'
+    f =open(filename, "w")
+
+    #Verde = py.pixelMatchesColor(510, 136, (17, 152, 80))
+    #Vermelho = py.pixelMatchesColor(510, 136, (204, 48, 48))
+    #RGB = list(py.pixel(514, 139))
+    RGB = list(py.pixel(x1, y1))
+    RGB = str(RGB[2])
+
+    #print(RGB)
+    f.write(RGB) 
+    f.close()
+
+    # >>> COMPARAÇÃO <<< #
+
+    s=open('Negocios_cruzam.txt', 'r', encoding="utf8")
+    try:
+        o=open('Negocios_cruzam2.txt','r', encoding="utf8")
+    except:
+        shutil.copy('Negocios_cruzam.txt', 'Negocios_cruzam2.txt')
+        o=open('Negocios_cruzam2.txt','r', encoding="utf8")
+
+    dadoS = s.read()
+    dadoO = o.read()
+
+    try:
+        if dadoS != dadoO:
+            if dadoS == '80':
+                bot = telepot.Bot('6372818731:AAFZXRVBq90-ufHCiTc5l_Mqj0ozYXHEV7o')
+                #bot.sendMessage(984798692, f'WDO_10R >> COMPRA <<')
+                bot.sendMessage(984798692, f'CRUZAMENTO_MMs >> COMPRA <<')
+                print('COMPRA')
+            elif dadoS == '48':
+                bot = telepot.Bot('6372818731:AAFZXRVBq90-ufHCiTc5l_Mqj0ozYXHEV7o')
+                #bot.sendMessage(984798692, f'WDO_10R >> VENDA <<')
+                bot.sendMessage(984798692, f'CRUZAMENTO_MMs >> VENDA <<')
+                print('VENDA')
+    except:
+         bot = telepot.Bot('6372818731:AAFZXRVBq90-ufHCiTc5l_Mqj0ozYXHEV7o')
+         bot.sendMessage(984798692, f'Falha no código, mas ainda funcionando. É bom conferir!')
+         
+         return
+    
+    s.close()
+    o.close()
+
+    shutil.copy('Negocios_cruzam.txt', 'Negocios_cruzam2.txt')
+    #print('.')
+
+while True:
+     # HORARIO DAS OPERAÇÕES
+        wdo4R()
+        sleep(2)
--- Batchs/SajNews.bat
+++ Batchs/SajNews.bat
@@ -1,15 +1,11 @@
 @echo    *********************************************************
-@echo    ***  COLETANDO E ENVIANDO INFORMACOES SOBRE SAJ   ***
+@echo    ***  COLETANDO E ENVIANDO INFORMACOES SOBRE O MUNDO  ***
 @echo    *********************************************************
 @echo off
 @echo off
 @echo   Por favor aguarde!  
 
 
-
-cd C:\Users\johnn\Google Drive\Pessoal\1_Programacao\Python Scripts\VsCode\1_Diversos
-
-
-py SajNews.py
-
+cd C:\Users\johnn\Google Drive\Pessoal\1_Programacao\Python Scripts\1 Python\Codes\1 VSCode\2 WebScraping
+py Noticias_SAJ_BBC.py
 
