23c1df72221f39ffac4c04bd0d050b6a020a4f6d
pberoschi <beroschi.peter@gmail.com>
2024-10-28 14:25:36 -0300

    .

--- 1 VSCode/5 Diversos/VisuYoutube.py
+++ 1 VSCode/5 Diversos/VisuYoutube.py
@@ -11,12 +11,19 @@
 
 
 
-# # driver.close()
+# driver.close()
+
+
+
 
 
 import os #importa o módulo os
+from time import sleep
 
 site = "https://www.youtube.com/watch?v=9Bw7dzMWeNw&t=55s"
 
 # os.system("\"C:\Path\To\chrome.exe\" -incognito www.jw.org/pt") #chama o chrome.exe no modo anônimo e abre o site. 
-os.system(f"\"C:\Program Files\Google\Chrome\Application\chrome.exe\" -incognito {site}") #chama o chrome.exe no modo anônimo e abre o site. 
+# os.system(f"\"C:\Program Files\Google\Chrome\Application\chrome.exe\" -incognito {site}") #chama o chrome.exe no modo anônimo e abre o site. 
+os.system(f"\"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe\" -inprivate {site}") #chama o edge no modo anônimo e abre o site.
+sleep(5)
+
